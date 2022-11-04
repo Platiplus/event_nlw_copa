67bf7542ba1dab5a76380fd0bae02c20cc24cbdb
Platiplus <quazarweb@gmail.com>
2022-11-04 19:12:17 -0300

    Adding integration between api and mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -6,10 +6,7 @@ import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@
 import { THEME } from './src/styles/theme'
 import { Loading } from './src/components/Loading'
 import { AuthContextProvider } from './src/contexts/AuthContext'
-// import { SignIn } from './src/screens/SignIn'
-// import { New } from './src/screens/New'
-// import { Find } from './src/screens/Find'
-import { Pools } from './src/screens/Pools'
+import { Routes } from './src/routes'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold })
@@ -18,8 +15,9 @@ export default function App() {
     <NativeBaseProvider theme={THEME}>
       <AuthContextProvider>
         <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-        { fontsLoaded ? <Pools /> : <Loading /> }
+        { fontsLoaded ? <Routes /> : <Loading /> }
       </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
+ 
